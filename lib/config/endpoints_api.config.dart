8528f6179352eb779f7600896f4acad1cf485c7e
VladiMoreno <124063072+VladiMoreno@users.noreply.github.com>
2024-06-18 23:48:52 -0600

    agregar funciones en el service

--- lib/config/endpoints_api.config.dart
+++ lib/config/endpoints_api.config.dart
@@ -1,3 +1,5 @@
 const getPokemonsPaginated = 'pokemon?limit={limitValue}&offset={offsetValue}';
 const getBasePokemonsPaginated =
     'evolution-chain?limit={limitValue}&offset={offsetValue}';
+const getPokemonInfo = 'pokemon/{pokemon_name}';
+const getPokemonSpecieInfo = 'pokemon-species/{pokemon_id}';
--- lib/modules/detail/detail.services.dart
+++ lib/modules/detail/detail.services.dart
@@ -1,6 +1,47 @@
 import 'package:get/get.dart';
 import 'package:pokedex/config/api.config.dart';
+import 'package:pokedex/config/endpoints_api.config.dart';
+import 'package:pokedex/utils/generate_final_url.util.dart';
+import 'package:pokedex/utils/logg_message.util.dart';
 
 class DetailServices extends GetxService {
   final apiService = APIService();
+
+  Future getPokemonInformation(String pokemonName) async {
+    try {
+      final params = <String, dynamic>{
+        'pokemon_name': pokemonName,
+      };
+
+      String finalUrl = generateFinalUrl(getPokemonInfo, params);
+
+      final response = await apiService.get(finalUrl);
+    } catch (e) {
+      printMessageParam(
+        message:
+            'Error en la función getPokemonInformation del archivo detail.service',
+        param: e,
+      );
+      rethrow;
+    }
+  }
+
+  Future getPokemonSpecieInformation(int pokemonId) async {
+    try {
+      final params = <String, dynamic>{
+        'pokemon_id': pokemonId,
+      };
+
+      String finalUrl = generateFinalUrl(getPokemonSpecieInfo, params);
+
+      final response = await apiService.get(finalUrl);
+    } catch (e) {
+      printMessageParam(
+        message:
+            'Error en la función getPokemonSpecieInformation del archivo detail.service',
+        param: e,
+      );
+      rethrow;
+    }
+  }
 }
