ad82d9d36244e07bfeef7ff02a8f82711ba6ac78
VladiMoreno <124063072+VladiMoreno@users.noreply.github.com>
2024-06-20 01:03:25 -0600

    agregar estructura del modulo

---
+++ lib/modules/pokemon_generation/pokemon_generation.services.dart
@@ -0,0 +1,3 @@
+import 'package:get/get.dart';
+
+class PokemonGenerationService extends GetxService {}
---
+++ lib/modules/pokemon_generation/pokemon_generation.view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PokemonGenerationView extends StatefulWidget {
+  const PokemonGenerationView({super.key});
+
+  @override
+  State<PokemonGenerationView> createState() => _PokemonGenerationViewState();
+}
+
+class _PokemonGenerationViewState extends State<PokemonGenerationView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
