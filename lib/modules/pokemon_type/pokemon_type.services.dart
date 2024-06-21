60dcdc9beddbbb8605d2c2d6064fb660bc3a2e6d
VladiMoreno <124063072+VladiMoreno@users.noreply.github.com>
2024-06-21 00:30:51 -0600

    agregar estructura general

---
+++ lib/modules/pokemon_type/pokemon_type.services.dart
@@ -0,0 +1,3 @@
+import 'package:get/get.dart';
+
+class PokemonTypeService extends GetxService {}
---
+++ lib/modules/pokemon_type/pokemon_type.view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PokemonTypeView extends StatefulWidget {
+  const PokemonTypeView({super.key});
+
+  @override
+  State<PokemonTypeView> createState() => _PokemonTypeViewState();
+}
+
+class _PokemonTypeViewState extends State<PokemonTypeView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
