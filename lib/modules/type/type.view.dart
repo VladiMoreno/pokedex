1bc36a1bd0d6d517771b4dfccfbc2b52916ae9da
VladiMoreno <124063072+VladiMoreno@users.noreply.github.com>
2024-06-20 18:41:02 -0600

    agregar estructura de modulo type

---
+++ lib/modules/type/type.view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TypeView extends StatefulWidget {
+  const TypeView({super.key});
+
+  @override
+  State<TypeView> createState() => _TypeViewState();
+}
+
+class _TypeViewState extends State<TypeView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
